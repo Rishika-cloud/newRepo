cc905f3da2e5d3ca654980b8e1c01ed57f9fccbf
Rishika-cloud <71088965+Rishika-cloud@users.noreply.github.com>
2023-01-27 23:22:51 +0530

    Done with current version

--- src/myClass.java
+++ src/myClass.java
@@ -4,6 +4,6 @@ public static void main(String [] args)
 {
     System.out.println("Hello Rishika, Welcone to newRepo);
     System.out.println("Hello Rishika, How are you?);
-    
+    System.out.println("Done ?);
 }
 }
