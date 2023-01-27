34dda0a568356091cc81187502028f9f2d32d301
Rishika-cloud <71088965+Rishika-cloud@users.noreply.github.com>
2023-01-27 22:18:38 +0530

    Update myClass.java

--- src/myClass.java
+++ src/myClass.java
@@ -3,5 +3,7 @@ class myClass
 public static void main(String [] args)
 {
     System.out.println("Hello Rishika, Welcone to newRepo);
+    System.out.println("Hello Rishika, How are you?);
+    
 }
 }
